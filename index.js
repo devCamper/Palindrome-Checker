94cbe41803c7bc411d91bcec5aee484e3a8b26a7
Git <git@github.com>
2019-01-07 11:51:03 +0100

    Implement palindrome(str)

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function palindrome(str) {
-    // Good luck!
-    return true;
+  // Good luck!
+  let newStr = str;
+  for(let i = 0; i < str.length; i++)
+    newStr = newStr.replace(/[^a-zA-Z0-9]/, '')
+  console.log(newStr)
+  let reversedStr = ''
+  for(let i = newStr.length-1; i >= 0; i--) {
+    reversedStr += newStr[i]
   }
-  
-  
-  
-  palindrome("eye");
+  console.log(reversedStr)
+  return newStr.toLowerCase() == reversedStr.toLowerCase()
+}
+
+
+
+palindrome("_eye");
+palindrome("A man, a plan, a canal. Panama")
